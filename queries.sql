1d9a001509d9ffef13551db47bb7abce1cb722d0
Matt <mwaterman29@gmail.com>
2022-11-28 16:50:34 -0500

    outline

---
+++ queries.sql
@@ -0,0 +1,27 @@
+/*
+Databases I Final - COMP 5730
+Patrick Kyoyetera and Matt Waterman 
+*/
+
+/*1. List titles of all books in ascending order. */
+
+/*2. Find the name of the author who wrote the book titled "On The Road". */
+
+/*3. List the title and author of books whose price is greater than $20. List your result in the 
+ascending order of the price. */
+
+/*4. Find the books that have the same title but different author(s). Each book title should 
+only be displayed once.*/
+
+/*5. Find the publisher that has the largest revenue in 2021. */
+
+/*6. List the title and price of all books written by the author of the best-selling book of 2021 
+(the book that has been sold the most number of copies in 2021). */
+
+/*7. Find the customer who has purchased every book written by Stephen King. */
+
+/*8. Insert a new author. */
+
+/*9. Increase $2 to those books whose price is lower than $10.*/
+
+/*10. Delete publishers and books they have published who are in Chicago. */
